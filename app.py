f428482f57abfb3fbb7cc958d45e8ec8a11401fe
VIHANGA22 <165758303+VIHANGA22@users.noreply.github.com>
2024-07-09 21:53:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,9 @@ for index, (col1, col2) in enumerate(rows):
                 elif info["type"] == "density_heatmap":
                     sales_by_country = sales_data.groupby('Country')['Sales'].sum().reset_index()
                     top_10_countries = sales_by_country.nlargest(10, 'Sales')
-                    d
+                    df_top_10_countries = sales_data[sales_data['Country'].isin(top_10_countries['Country'])]
+                    fig = getattr(px, info["type"])(df_top_10_countries, x=info.get("x"), y=info.get("y"), title=info.get("title"), color_continuous_scale=info.get("color_scale"))
+                else
 
 
 
