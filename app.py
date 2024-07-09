a96f221f3de63734c9a1262c1d53e2fd7f494b91
VIHANGA22 <165758303+VIHANGA22@users.noreply.github.com>
2024-07-09 21:15:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+# Setting page configuration
+st.set_page_config(
+    page_title="Sales Dashboard",
+    page_icon=":chart_with_upwards_trend:",
+    layout="wide"
+)
+
